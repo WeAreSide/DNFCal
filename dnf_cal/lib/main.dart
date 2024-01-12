161159bed82f2787ef07d9cea285f9c7198ee2ff
dayjack <frostjanna12@gmail.com>
2024-01-12 20:14:20 +0900

    [Assets/#7] 폰트 추가

--- dnf_cal/lib/main.dart
+++ dnf_cal/lib/main.dart
@@ -49,7 +49,8 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'You have pushed the button many times:',
+              style: TextStyle(fontFamily: 'DNFForgedBlade', fontWeight: FontWeight.w500),
             ),
             Text(
               '$_counter',
