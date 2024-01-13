463dae79cb8a1c953ced3c562fe151f4907bb753
dayjack <frostjanna12@gmail.com>
2024-01-13 13:49:04 +0900

    [Feat/#9] BottomNavigationBar 기본 구현

---
+++ dnf_cal/lib/screens/CharacterSearchPage.dart
@@ -0,0 +1,25 @@
+import 'package:dnf_cal/main.dart';
+import 'package:flutter/material.dart';
+
+class CharacterSearchPage extends StatefulWidget {
+  const CharacterSearchPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _CharacterSearchPageState();
+  }
+}
+
+class _CharacterSearchPageState extends State<CharacterSearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child:
+        GestureDetector(
+            child: Text('캐릭터 검색 페이지')
+        )
+      ),
+
+    );
+  }
+}
