79d19039c5210d8e0501ebc1115c5c5909c3246a
김기범 <gimgibeom@gimgibeom-ui-MacBookAir.local>
2024-01-16 20:28:44 +0900

    [Refactor/#31] 추가 DnfText.dart

---
+++ dnf_cal/lib/widgets/global/DnfText.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+// 자주 사용되는 Text 위젯을 미리 정의해둔다.
+// default 값을 주도록해서 인자가 생략되도 가능하도록 한다.
+class DnfText extends StatelessWidget {
+  const DnfText(
+    this.text, {
+    Key? key,
+    this.color,
+    this.fontSize,
+    this.fontWeight,
+    this.fontFamily,
+    this.textAlign,
+    this.maxLines,
+    this.overflow,
+  }) : super(key: key);
+
+  final String text;
+  final Color? color;
+  final double? fontSize;
+  final FontWeight? fontWeight;
+  final String? fontFamily;
+  final TextAlign? textAlign;
+  final int? maxLines;
+  final TextOverflow? overflow;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        color: color ?? Colors.white,
+        fontSize: fontSize ?? 12,
+        fontWeight: fontWeight ?? FontWeight.w500,
+        fontFamily: fontFamily ?? 'DNFForgedBlade',
+      ),
+      textAlign: textAlign ?? TextAlign.start,
+      maxLines: maxLines,
+      overflow: overflow,
+    );
+  }
+}
