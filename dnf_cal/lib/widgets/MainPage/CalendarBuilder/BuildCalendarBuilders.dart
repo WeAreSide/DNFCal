7d3c3b812c189359d71695f5378bb0275af32919
김기범 <gimgibeom@gimgibeom-ui-MacBookAir.local>
2024-01-23 19:03:58 +0900

    [Feat/#54] 달력 디자인 수정

--- dnf_cal/lib/widgets/MainPage/CalendarBuilder/BuildCalendarBuilders.dart
+++ dnf_cal/lib/widgets/MainPage/CalendarBuilder/BuildCalendarBuilders.dart
@@ -5,6 +5,73 @@ import 'package:table_calendar/table_calendar.dart';
 
 CalendarBuilders buildCalendarBuilders() {
   return CalendarBuilders(
+    defaultBuilder: (context, day, focusedDay) {
+      return Container(
+        decoration: BoxDecoration(
+          color: Color(0x10ffffff),
+        ),
+        child: Container(
+          margin: EdgeInsets.only(top: 1),
+          decoration: BoxDecoration(
+            color: Color(0xff0f172f),
+          ),
+          child: Align(
+            alignment: Alignment.topCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 5),
+              child: DnfText(
+                '${day.day}',
+              ),
+            ),
+          ),
+        ),
+      );
+    },
+    todayBuilder: (context, day, focusedDay) {
+      return Container(
+        decoration: BoxDecoration(
+          color: Color(0x0fffffff),
+        ),
+        child: Container(
+          margin: EdgeInsets.only(top: 1),
+          decoration: BoxDecoration(
+            color: Color(0xff0f172f),
+          ),
+          child: Align(
+            alignment: Alignment.topCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 5),
+              child: DnfText(
+                '${day.day}',
+              ),
+            ),
+          ),
+        ),
+      );
+    },
+    outsideBuilder: (context, day, focusedDay) {
+      return Container(
+        decoration: BoxDecoration(
+          color: Color(0x0fffffff),
+        ),
+        child: Container(
+          margin: EdgeInsets.only(top: 1),
+          decoration: BoxDecoration(
+            color: Color(0xff0f172f),
+          ),
+          child: Align(
+            alignment: Alignment.topCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 5),
+              child: DnfText(
+                '${day.day}',
+                color: CustomColor.darkGray(),
+              ),
+            ),
+          ),
+        ),
+      );
+    },
     markerBuilder: (context, day, events) {
       if (events.isNotEmpty) {
         int levelValue = int.parse(events[0].toString()) ~/ 120;
@@ -30,10 +97,12 @@ CalendarBuilders buildCalendarBuilders() {
             levelColor = Colors.red;
         }
         return Positioned(
-          bottom: 1,
+          bottom: 3,
           child: DnfText(
             '${events[0]}',
             color: levelColor,
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
           ),
         );
       }
